564f2cb99ef9264db461616c1366155f1d71128b
deijomes <deibermndoza@gmail.com>
2025-08-05 12:41:27 -0500

    style plantilla samo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { BienvenidaPage } from './pages/bienvenida/bienvenida.page';
 import { ConfiguraRecaudoPage } from './pages/configura-recaudo/configura-recaudo.page';
 import { ConfiguraFacturacionPage } from './pages/configura-facturacion/configura-facturacion.page';
+import { PruebaComponent } from './prueba/prueba.component';
 
 export const routes: Routes = [
   { path: '', component: BienvenidaPage },
   { path: 'configuraRecaudo', component: ConfiguraRecaudoPage },
-  { path: 'configuraFacturacion', component: ConfiguraFacturacionPage }
+  { path: 'configuraFacturacion', component: ConfiguraFacturacionPage },
+  { path: 'prueba', component: PruebaComponent }
 ];
